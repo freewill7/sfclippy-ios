299e8c47699a54ab92d5cf81dec6571793e73f64
William Lee <pseudonym.wlee@gmail.com>
2018-02-18 22:28:05 +0000

    Introduced heading view for detailed result view.

---
+++ sfclippy/SfHeadingView.swift
@@ -0,0 +1,57 @@
+//
+//  SfHeadingView.swift
+//  sfclippy
+//
+//  Created by William Lee on 18/02/2018.
+//  Copyright © 2018 William Lee. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class SfHeadingView: UIView {
+    @IBOutlet weak var labelMain: UILabel!
+    @IBOutlet var contentView: UIView!
+    
+    /**
+     The message to display in the draggable view.
+     */
+    @IBInspectable var message : String? {
+        didSet {
+            self.labelMain.text = message
+        }
+    }
+    
+    @IBInspectable var fontColor : UIColor? {
+        didSet {
+            self.labelMain.textColor = fontColor
+        }
+    }
+    
+    private func commonInit( ) {
+        let bundle = Bundle(for: SfHeadingView.self)
+        bundle.loadNibNamed("SfHeadingView", owner: self, options: nil)
+        addSubview(contentView)
+        contentView.frame = self.bounds
+        
+        contentView.backgroundColor = self.backgroundColor
+        /*
+        self.labelMessage.text = message
+        self.labelMessage.textColor = self.backgroundColor
+        self.imageAssistant.tintColor = self.backgroundColor
+        self.viewMoveable.backgroundColor = self.tintColor
+ */
+        
+    }
+    
+    override init( frame : CGRect ) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+
+}
